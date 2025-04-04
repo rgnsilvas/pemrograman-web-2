4306773a9c2c2c2b1f0e20e9ddaec633446b7c05
seasil <rgnasisil@gmail.com>
2025-04-04 12:02:58 +0800

    first commit

---
+++ modul1/PRAK101.php
@@ -0,0 +1,5 @@
+<?php
+    echo"Hello World in PHP<br>";
+    echo"Nama Praktikan : {Regina Silva Maharatini}<br>";
+    echo"NIM : {2310817220011}";
+?>
---
+++ modul1/PRAK102.php
@@ -0,0 +1,8 @@
+<?php
+    $jarijari = 4.2;
+    $tinggi = 5.4;
+    $phi = 3.14;
+    $volume = $phi * $jarijari * $jarijari * $tinggi;
+
+    echo number_format ($volume, 3); 
+?>
